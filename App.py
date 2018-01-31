36aafe5ce60edfd7f4e9646119e43e0e26d183d2
Tiger Creative <Workstation3@dhcp-10-10-128-101.knoles.pacific.edu>
2018-01-30 16:05:48 -0800

    test

--- App.py
+++ App.py
@@ -18,6 +18,16 @@ THREADING = False
 FLASK_DEBUG = True
 
 
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+app.after_request(add_cors_headers)
+
 '''
 def _load_atlas():
     atlas_file = file_manager.find_existing_file(query_title=config.ATLAS)
